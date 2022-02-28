861a9911914e59ab4cd4bdbfb46e0af1bb9c13af
Seth Reeise <southsc22@gmail.com>
2022-02-28 10:40:26 -0500

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,18 @@
+// fs stands for file system
+// this allows you to access the files on the web server
+const fs = require('fs');
+
+// read file sync returns a string
+// using json parse method to return an object instead
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // Get travel page 
 const travel = (req, res) => {
     // redner the travel page and add title
     pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    // pageTitle = process.env.description + ' - Travel';
+    // pass title and trips to hbs page
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {
